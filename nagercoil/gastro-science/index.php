b7e7585b4e11d2898f05f5af8aec3a2a53bc6603
aamisubha <sbhadipchanda@gmail.com>
2026-02-07 15:10:50 +0530

    GA Code added

--- nagercoil/gastro-science/index.php
+++ nagercoil/gastro-science/index.php
@@ -37,7 +37,15 @@
     })(window, document, 'script', 'dataLayer', 'GTM-NPDG225B');
   </script>
   <!-- End Google Tag Manager -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17011074142"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'AW-17011074142');
+</script>
 </head>
 
 <body>
